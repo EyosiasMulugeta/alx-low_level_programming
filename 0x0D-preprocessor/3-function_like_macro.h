cb28f346013b117bbc822ef2b57733c3f6269d20
EyosiasMulugeta <86252988+EyosiasMulugeta@users.noreply.github.com>
2023-01-16 22:16:40 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,6 @@
-#include <stdio.h> 
-#include <math.h> 
-int main() { 
-double x; 
-x = 5.5; 
-printf("The absolute value of 5.5 is %f
-", ABS(x)); 
-return 0; 
-}
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
